8bb10e52e6c888e04febcf828932149b0c6988a7
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-08-22 10:44:39 +0530

    Add files via upload

---
+++ python task2.py
@@ -0,0 +1,44 @@
+#number is divisible by 3 and 
+# num=int(input("Enter a number:"))
+# if num%3==0 and num%5==0:
+#     print("The number is divisible by both 3 and 5")
+# else:
+#     print("The number is not divisible by both 3 and 5")
+
+# num1=int(input("Enter first number:"))
+# num2=int(input("Enter second number:"))
+# if num1>num2:
+#     print("num1 is greater")
+# else:
+#     print("num2 is greater")
+
+
+# Score=int(input("Enter your Score:"))
+# if Score>=90:
+#     print("Grade A")
+# elif Score>=80:
+#     print("Grade B")
+# elif Score>=70:
+#     print("Grade C")
+# else:
+#     print("Fail")
+
+# num=int(input("Enter a number:"))
+# if num>0:
+#     if num%2==0:
+#         print("even number")
+#     else:
+#         print("odd number")
+#     print("Positive number")
+# else:
+#     print("Negative number")
+
+# age=int(input("Enter your age:"))
+# if age<13:
+#     print("You are a child")
+# elif age>=13 and age<=19:
+#     print("You are a teenager")
+# else:
+#     print("You are an adult")
+
+    
