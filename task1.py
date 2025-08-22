c0682e264539cb8cabe2df84785ca438d147e33a
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-08-22 11:02:40 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,74 @@
+# x=15
+# y=18
+# temp=x
+# x=y
+# y=temp
+# print("x:",x)
+# print("y:",y)
+
+
+a = input("Enter first value: ")
+b = input("Enter second value: ")
+c = input("Enter third value: ")
+print("Value 1:", a, ", Type:", type(a))
+print("Value 2:", b, ", Type:", type(b))
+print("Value 3:", c, ", Type:", type(c))
+
+# a=9
+# b=2
+# print("addition of a and b:",a+b)
+# print("subtraction of a and b:",a-b)
+# print("multiplication of a and b:",a*b)
+# print("division of a and b:",a/b)
+# print("modulas of a and b:",a%b)
+# print("exponent of a and b:",a**b)
+# print("floor division of a and b:",a//b)
+
+
+# salary = float(input("Enter your current salary: "))
+# hike = salary * 0.15
+# new_salary = salary + hike
+# print("Old Salary:", salary)
+# print("New Salary after 15% hike:", new_salary)
+
+
+# num=int(input("Enter a number:"))
+# if num%2==0:
+#     print("Even number")
+# else:
+#     print("odd number")
+
+
+# x=20
+# x+=5
+# print(x)
+# x-=3
+# print(x)
+# x*=2
+# print(x)
+# x/=4
+# print(x)
+# x//3
+# print("floor:",x)
+# x%2
+# print(x)
+# x**=2
+# print(x)
+
+
+
+# pi = 3.14159
+# radius = float(input("Enter the radius of the circle: "))
+# area = pi * radius ** 2
+# perimeter = 2 * pi * radius
+# print(f"Area of the circle: {area}")
+# print(f"Perimeter (Circumference) of the circle: {perimeter}")
+
+
+# char = input("Enter a single character: ")
+# if len(char) == 1:
+#     ascii_value = ord(char)
+#     print(f"The ASCII value of '{char}' is {ascii_value}")
+# else:
+#     print("Please enter exactly one character.")
+
