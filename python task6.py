25d1229c1750c8416639a47a65a216da6814e61f
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-08-22 10:47:11 +0530

    Add files via upload

---
+++ python task6.py
@@ -0,0 +1,76 @@
+# numbers = [1, 2, 6, 5, 4]
+# for num in numbers:
+#     if num % 2 == 0:
+#         print(f"First even number is: {num}")
+#         break
+
+# correct_password = "Vishalika"
+# for attempt in range(3):
+#     password = input("Enter password: ")
+#     if password == correct_password:
+#         print("Access granted")
+#         break
+# else:
+#     print("locked out")
+
+# while True:
+#     user_input = input("vishalika(or 'exit' to quit): ").lower()
+#     if user_input == "exit":
+#         print("Loop stopped")
+#         break
+#     print(f"You typed: {user_input}")
+
+
+# numbers = [5, -3, 7, -1, 0, 8]
+# for num in numbers:
+#     if num < 0:
+#         continue
+#     print(num)
+
+
+# word = "education"
+# vowels = "aeiou"
+
+# for char in word:
+#     if char in vowels:
+#         continue
+#     print(char,end=" ")
+
+
+# for num in range(1, 51):
+#     if num % 3 ==0 or num % 5 !=0:
+#         continue
+#     print(num)
+
+# class User:
+#     pass
+# u1 = User()
+# print(type(u1))
+
+
+# for i in range(1,6):
+#     pass
+
+
+# numbers = [2, 5, 0, 8, 12, 51, 18, 20]
+# for num in numbers:
+#     if num % 2 != 0:  
+#         continue
+#     if num >= 50:     
+#         break
+#     if num == 0:     
+#         pass
+#     else:
+#         print(num)
+
+
+# words = ["hi", "cat", "wait", "dog", "end", "zebra"]
+# for word in words:
+#     if len(word) < 3:  
+#         continue
+#     if word == "end":  
+#         break
+#     if word == "wait": 
+#         pass
+#     else:
+#         print(word)
