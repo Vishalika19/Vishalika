b718378fd5b877882c61906025365b4e248622e9
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-08-22 10:38:59 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,65 @@
+# def add():
+#     a=18
+#     b=15
+#     print(a+b)
+# add()
+
+# def add(a,b):
+#     print(a+b)
+# add(10,15)
+
+# def add(a,b):
+#     return a//b
+    
+# print(add(20,30)+10)
+
+# def sub(x,y):
+#     return x-y
+# print(sub(10,5)+10)
+
+
+# def evenorodd():
+#     for i in range(1,11):
+#         if i%2==0:
+#             return 'even'
+#         else:
+#             return 'odd'
+# result=(01,2,3,4,5,6,7,8,9,10)
+# print(evenorodd(result))
+
+
+# def greetings():
+#     name=input("Enter your name:")
+#     print('welcome',name,'to 10000 coders')
+# greetings()
+
+# def greetings():
+#     name=input("Enter your name:")
+#     print(f'welcome {name} to 10000 coders')
+# greetings()
+
+
+# def numbers():
+#     for i in range(1,26):
+#         print(i)
+# numbers()
+
+
+# def greetings(name,course,institute):
+#     print(f'Welcome {name} to {course} at {institute}')
+# greetings('Vishalika','Python full stack','10000 coders')
+
+# def numbers(a,b):
+#     print(f'addition:',a+b)
+#     print(f'subtraction:',a-b)
+#     print(f'multiplication:',a*b)
+#     print(f'division',a/b)
+# numbers(25,15)
+
+# def strings(str1,str2,number):
+#     print(f'concatenation:',str1+str2)
+#     print(f'replication:',str2*5)
+# strings('hi','hello',5)
+
+
+
