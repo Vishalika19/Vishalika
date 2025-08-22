4a1f06470e11a65bb941adcdc75f261fc169d70a
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-08-22 11:03:56 +0530

    Add files via upload

---
+++ task11.py
@@ -0,0 +1,10 @@
+# string = input("Enter a string: ")
+# reversed_str = ""
+# length = 0
+# for _ in string:
+#     length += 1
+# while length > 0:
+#     reversed_str += string[length - 1]
+#     length -= 1
+# print("Reversed string:", reversed_str)
+
