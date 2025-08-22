77bc8581b8b0833be03f4b2e971a37f7445bb918
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-08-22 10:47:38 +0530

    Add files via upload

---
+++ python task7.py
@@ -0,0 +1,60 @@
+# for i in range(1,51):
+#     if i%3==0:
+#         continue
+#     print(i)
+
+# while True:
+#     num=int(input("Enter a number:"))
+#     if num%11==0:
+#         print(' exit the loop')
+#         break
+#     else:
+#         print('Try again')
+
+
+# num=200
+# while num>=2:
+#     print(num)
+#     num-=2
+
+
+# while True:
+#     password=input("Enter a password:")
+#     if password=='admin123':
+#         print('Access granted')
+#         break
+#     else:
+#         print('Access not granted')
+    
+# num=int(input("Enter a number:"))
+# for i in range(1,11):
+#     print(num,'x',i,'=',num*i)
+
+
+# for i in range(10):
+#     num = int(input("Enter a number: "))
+#     if num >= 0:   
+#         print("Positive number:",num)
+
+
+
+# N=int(input("Enter a number: "))
+# i=0
+# for i in range(1, N+1):
+#     odd = 2 * i-1  
+#     i+=odd
+# print("Sum of first", N, "odd numbers is:",i)  
+
+
+# for num in range(1, 51):  
+#     if num <= 1:
+#         continue
+#     for i in range(2, num):
+#         if num % i == 0:
+#             break   
+#     else:
+#         print(num) 
+
+
+
+    
