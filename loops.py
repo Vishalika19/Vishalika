042fc7940c55e908cd6573636e0ad2c460cf5021
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-08-22 10:41:44 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,50 @@
+# list1=[1,2,3,4,5]
+# for i in list1:
+#     print("Hello",i)
+
+
+# list1=[1,2,3,4,5,6,7,8,9,10]
+# for i in list1:
+#     print("Hello world",i)
+
+
+# for i in range(1,11,3):
+#     print(i)
+
+# list1=list(range(10,50))
+# print(len(list1))
+
+
+# list1=["apple","banana","mango","orange"]
+# print(len(list1))
+# for i range(len(list1)):
+#      print(i,list[i])
+
+# list1=["apple","banana","mango","orange"]
+# print(len(list1))
+# for i  in range(len(list1)):
+#      print(i,list1[i])
+
+
+# string1="vishalika"
+# for i in range(len(string1)):
+#     print(i,string1[i])
+
+# for i in range(5):
+#     for j in range(5):
+#         print("i value:",i, "j value:",j)
+
+# list1=["Ledalla"]
+# list2=["Vishnu vardhan","Ratnamala","Sris shylika","Vishalika"]
+# for i in list1:
+#     for j in list2:
+#         print(i,j)
+
+
+# i=15
+# while i<20:
+#     print("Vishalika","Sai")
+#     i=i-1
+
+# while True:
+#     print("Hello world")
