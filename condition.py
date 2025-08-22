a186abb9cc43d9897284daaf99d45e16b50e1a21
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-08-22 10:38:21 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,119 @@
+# job = False
+# if job==True:
+#     print("you have a job")
+# else:
+#     print("Go to institute")
+
+
+# job = True
+# money=True
+# skills=True
+# if(job==True and money==True and skills==True):
+#     print("Your are ready with your package ")
+# if job==True:
+#     print("you have a job")
+# elif money==True:
+#     print("Doing Business")
+# elif skills==True:
+#     print("Doing Freelanicing")
+# else:
+#     print("Go to institute")
+
+
+# Coffee = 100
+# Tea = 250
+# money = 300
+# if Coffee > money:
+#     print("You can buy coffee")
+# elif Tea < money:
+#     print("You can buy tea")
+# else:
+#     print("water bottle")
+
+# print("Enter your age:")
+# age = int(input())20
+# if( age >= 18):
+#     print("Eligible for voting")
+# else:
+#     print("Not eligible for voting")
+
+
+# percentage = int(input("Enter your percentage:"))
+# if (percentage >= 90):
+#     print("o grade is obatained")
+# elif (percentage >= 80):
+#     print("A grade is obtained")
+# elif (percentage >= 75):
+#     print("B grade is obatained")
+# elif (percentage >= 60):
+#     print("C grade is obtained")
+# elif (percentage <= 50):
+#     print("D grade is obtained")
+# else:
+#     print("f grade is obtained")
+
+
+# a = int(input("Enter a year: "))
+# if (a%4==0 and a%100!=0):
+#     print("it is a leap year")
+# else:
+#     print("it is not a leap year")
+
+
+# num = int(input("Enter a number:"))
+# if num > 0:
+#     print("The number is positive")
+# else:
+#     print("The number is negative")
+
+
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print(" Even number")
+# else:
+#     print("Odd number")
+
+
+# Grade=int(input("Enter your marks:"))
+# if Grade >= 90:
+#     print("fabolous")
+# elif Grade >= 80:
+#     print("Excellent")
+# elif Grade >= 70:
+#     print("very Good")
+# elif Grade >= 60:
+#     print("Good")
+# elif Grade >= 50:
+#     print("Average")
+# elif Grade >= 35:
+#     print("Below Average")
+# else:
+#     print("Fail")
+
+
+# age=int(input("Enter your age:"))
+# if age<=12:
+#     print("You are a child")
+# elif age>=12 and age<18:
+#     print("You are a minor")
+# elif age>= 18 and age<60:
+#     print("You are a major")
+# elif age>=60 and age<100:
+#     print("You are a senior citizen")
+# else:
+#     print("Old age") 
+        
+# a=int(input("a:"))
+# b=int(input("b:"))
+# if a > b:
+#     print("a is greater")
+# elif a < b:
+#     print("b is greater")
+# else:
+#     print("a is equal to b")
+
+
+
+
+
+
