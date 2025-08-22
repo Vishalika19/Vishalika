9566bbf8c559318232c71cef8d6806de9806e1ce
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-08-22 10:40:39 +0530

    Add files via upload

---
+++ list methods.py
@@ -0,0 +1,26 @@
+# list1=[1,2,3,4,5,]
+# #append
+# list1.append(6)
+# print(list1)
+
+# #insert
+# list1.insert(2,2.5)
+# print(list1)
+# #extend
+# list1.extend([7,8,9])
+# print(list1)
+
+# #remove
+# list1.remove(2.5)
+# print(list1)
+
+# #pop
+# list1.pop(6)
+# print(list1)
+
+# list1.insert(6,7)
+# print(list1)
+
+# #clear
+# list1.clear()
+# print(list1)
