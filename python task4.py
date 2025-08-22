ae72250192127bc52b0b64f78453781d6f1b821e
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-08-22 10:45:10 +0530

    Add files via upload

---
+++ python task4.py
@@ -0,0 +1,100 @@
+# i=1
+# while(i<=5):
+#     print(i)
+#     i+=1
+
+# i=1
+# while(i<=10):
+#     if i%2==0:
+#         print(i)
+#     i+=1
+
+# num=1
+# sum_list=[0]
+# while num<=10:
+#     sum_list[0]+=num
+#     num+=1
+# print("sum",sum_list[0])
+
+
+# i=10
+# while(i>0):
+#     print(i)
+#     i-=1
+
+# for i in range(1,10):
+#     print(i)
+
+# word="banana"
+# for char in word:
+#     print(char)
+
+# for i in range(1,6):
+#     print(i**2)
+
+# for i in range(1,20):
+#     print(i*3)
+
+# list=[2,5,8,9,10]
+# for i in list:
+#     if i%2==0:
+#         print(i)
+
+# for i in range(1,11):
+#     print("2 x",i,"=",2*i)
+
+# words=["hi","ok"]
+# for word in words:
+#     for char in word:
+#         print(char)
+
+
+# for i in range(1,11):
+#     if i%2==0:
+#         print(i,"Even")
+#     else:
+#         print(i,"Odd")
+
+# word=input("Enter a word:")
+# for char in word:
+#     if char.lower()in "aeiou":
+#         print(char)
+
+# word=input("Enter a word:")
+# for char in word:
+#     if char.lower()in "aeiou":
+#         print(char)
+
+# word=input("Enter a word:")
+# for char in word:
+#     if char.lower()in "aeiou":
+#         print(char)
+
+# word=input("Enter a word:")
+# count=0
+# for char in word:
+#     if char.lower()in "aeiou":
+#         count+=1
+#         print("No.of vowels:",count)
+
+# for i in range(5):
+#     print("Hello")
+
+# for i in range(3):
+#     color=input("Enter your favourite color:")
+#     print("Favourite color",color)
+
+# word="banana"
+# letter=input("Enter letter to count:")
+# count=0
+# for char in word:
+#     if char==letter:
+#         count+=1
+# print(f"The letter'{letter}'appears {count} times in '{word}'")
+
+# for i in range(1,11):
+#     if i<5:
+#         print(i,"Small")
+#     else:
+#         print(i,"Big")
+
