788dc122b9ebaf542883fc975d666927e62d1b93
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-08-22 10:58:26 +0530

    Add files via upload

---
+++ str build in fun.py
@@ -0,0 +1,106 @@
+# a="vishalika"
+# print(a.upper())
+
+# s="A"
+# print(s.lower())
+# if s.lower():
+#     print("it is a lower string")
+# else:
+#     print("it is a upper string")
+
+
+# s="a"
+# print(s.upper())
+# if s.upper():
+#     print("it is a upper string")
+# else:
+#     print("it is a lower string")
+
+
+# a="vishalika"
+# if a.islower():
+#     print("upper")
+# else:
+#     print("lower")
+
+
+# a="ViSAhaLIka"
+# print(a.swapcase())
+
+
+# a="vishalika"
+# a=a.swapcase()
+# print(a)
+
+
+# a="vishalika"
+# a=a.capitalize()
+# print(a)
+
+
+# a="vishalika ledalla"
+# a=a.title()
+# print(a)
+
+
+# a="vishalika ledalla"
+# print("lower:",a.lower())
+# print("upper:",a.upper())
+# print("is lower:",a .islower())
+# print("is Upper:",a.isupper())
+# print("swap:",a.swapcase())
+# print("capitalize:",a.capitalize())
+# print("title:",a.title())
+
+
+# a="  vishalika  "
+# print(a.strip())
+
+
+# a="   vishalika"
+# a=a.lstrip()
+# print(a)
+
+
+# a="vishalika   "
+# a=a.rstrip()
+# print(a)  
+
+
+# a="i am a java learner"
+# b="i am a python learner"
+# print(a.replace("java","python"))
+
+# a="i am a java learner"
+# print(a.replace("java","python"))
+
+
+# a="i am a java learner"
+# print(a.replace("a","b"))
+
+# a="i am a java learner"
+# print(a.replace("java","python"))
+
+
+# a="i am a java learner"
+# print(a.replace("a","b"))
+
+
+
+# a="vishalika"
+# print(a.find("a"))
+
+
+# a="vishalika"
+# print(a.find("f"))
+
+# a="vishalika"
+# a=a.rfind("a")
+# print(a)
+
+
+# a="vishalika"
+# a=a.count("i")
+# print(a)
+
+
