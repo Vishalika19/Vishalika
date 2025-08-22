dd4351e0cfdf83948c03d233152cbac548ea0241
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-08-22 11:01:01 +0530

    Add files via upload

---
+++ swap2.py
@@ -0,0 +1,18 @@
+x="v"
+y="i"
+z="s"
+a="h"
+b="a"
+c="l"
+d="i"
+e="k"
+f="a"
+print(x+ "\n")
+print(y+ "\n")
+print(z+ "\n")
+print(a+ "\n")
+print(b+ "\n")
+print(c+ "\n")
+print(d+ "\n")
+print(e+ "\n")
+print(f+ "\n")
