50d3d478a411dd786a820b62b58f313dbb639abc
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-08-22 10:45:45 +0530

    Add files via upload

---
+++ python task5.py
@@ -0,0 +1,57 @@
+# bottles = int(input("Enter number of juice bottles: "))
+# regular_customer = input("Are you a regular customer? (yes/no): ").lower()
+# if bottles > 2:
+#     if regular_customer == "yes":
+#         discount = 15
+#     else:
+#         discount = 5
+# else:
+#     discount = 0
+# print(f"Discount: {discount}%")
+
+
+# fuel_level = float(input("Enter fuel level percentage: "))
+# vehicle_type = input("Enter vehicle type (car/bike): ")
+# if fuel_level < 25:
+#     if vehicle_type == "car":
+#         print("Refuel soon at a petrol station.")
+#     elif vehicle_type == "bike":
+#         print("Refuel at nearest pump.")
+#     else:
+#         print("Check vehicle type.")
+# else:
+#     print("Fuel level is sufficient.")
+
+
+# grade = float(input("Enter student's grade: "))
+# income = float(input("Enter annual income (in lakhs): "))
+# if grade > 85:
+#     if income < 3:
+#         print("Eligible for full scholarship.")
+#     elif 3 <= income <= 6:
+#         print("Eligible for half scholarship.")
+#     else:
+#         print("No scholarship.")
+# else:
+#     print("No scholarship.")
+
+# light_color = input("Enter light color: ").lower()
+# print("Go" if light_color == "green" else "stop")
+
+# age=int(input("Enter your age:"))
+# print("Adult ticket" if age>18 else "child ticket")
+
+
+# location = input("Enter location (local):")
+# print(f"Delivery fee: Rs{20 if location == 'local'else 50}")
+
+# temperature=int(input("Enter your temperature:"))
+# print("High" if temperature>=100 else "Normal")
+
+
+# hour = int(input("Enter hour (0-23): "))
+# print("Good Morning" if hour < 12 else "Good Afternoon" if hour < 17 else "Good evening")
+
+
+
+               
