c9e9194997d1b7d283df6ea329caa6d6799a6dc6
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-08-22 10:49:09 +0530

    Add files via upload

---
+++ set methods.py
@@ -0,0 +1,24 @@
+# set1={1,2,3,4,5}
+# set2={4,5,6,7,8}
+#add
+# set1.add(7)
+# print(set1)
+
+#remove
+# set1.remove(4)
+# print(set1)
+
+# set1.remove(7)
+# print(set1)
+
+#discard
+# set1.discard(7)
+# print(set1)
+
+#union
+# print(set1.union(set2))
+
+#intersection
+# print(set1.intersection(set2))
+
+
