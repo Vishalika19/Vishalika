951faddfe30c34a63040c060f25f8a07756ef230
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-08-22 10:37:08 +0530

    Add files via upload

---
+++ bitwise.py
@@ -0,0 +1,16 @@
+'''a=[3,5,6]
+b=a
+c=[3,5,6]
+print(a is b)
+print(a is c)
+print(a == c)
+print(3 is not c)
+print(a == b)
+print(3 is not b)'''
+print(5 & 4)
+print(5 | 4)
+print(5 ^ 4)
+print(~7)
+print(6 << 2)
+print(8 >> 2)
+print(~-5)
