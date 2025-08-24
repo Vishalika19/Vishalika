207f40c0a5daf3b80ce8ee8a933da314845dab7e
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-08-24 19:08:54 +0530

    Add files via upload

---
+++ text3_random.py
@@ -0,0 +1,20 @@
+# import random
+# print(random.randint(1,100))
+
+
+# import random
+# list1=["apple","banana","mango","orange"]
+# print(random.choice(list1))
+
+# import random
+# list1=[1,2,3,4,5]
+# random.shuffle(list1)
+# print(list1)
+
+# import random
+# for i in range(5):
+#     print(random.randint(10,50))
+
+import random
+rand_float=random.random()
+print(rand_float)
