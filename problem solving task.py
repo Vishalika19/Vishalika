f128a855c901a45bea700a1dfc31f8a010d604f2
Ledalla Vishalika <vishalikaledalla@gmail.com>
2025-09-06 14:12:29 +0530

    Add files via upload

---
+++ problem solving task.py
@@ -0,0 +1,26 @@
+# string=input('enter a string:')
+# reversed_str=""
+# length=0
+# for _ in string:
+#     length+=1
+# while length>0:
+#     reversed_str+=string[length-1]
+#     length-=1
+# print("Reversed string:",reversed_str)
+
+
+# marks = [400, 450, 530, 550, 570]
+# total = 0
+# for mark in marks:
+#     total += mark
+#     average = total/5
+# print("Average marks:", average)
+
+
+a = 5
+b = 5
+c = 5
+if (a + b > c) and (a + c > b) and (b + c > a):
+    print("The sides form a triangle")
+else:
+    print("The sides do not form a triangle")
